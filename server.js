6ab94d721c4559d151939c9ffd39d870e6d41a3c
captainslow <jjkew9198@gmail.com>
2020-08-15 15:05:24 +0800

    dyanmic port

--- server.js
+++ server.js
@@ -150,9 +150,10 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
   });
 });
 
