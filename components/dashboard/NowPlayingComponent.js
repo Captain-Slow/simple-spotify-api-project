d6a643fccdb1039d836cf5940cde39696914be64
captainslow <jjkew9198@gmail.com>
2020-08-16 16:05:24 +0800

    add device icon

--- components/dashboard/NowPlayingComponent.js
+++ components/dashboard/NowPlayingComponent.js
@@ -6,6 +6,9 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import RefreshIcon from "@material-ui/icons/Refresh";
+import ComputerIcon from "@material-ui/icons/Computer";
+import SmartphoneIcon from "@material-ui/icons/Smartphone";
+import SpeakerIcon from "@material-ui/icons/Speaker";
 
 import { fetchUserPlayBack } from "../../redux/actions/app";
 
@@ -41,6 +44,27 @@ function NowPlayingComponent(props) {
               <Typography color="inherit" variant="body2" gutterBottom>
                 {playBack.data.item.artists_modified}
               </Typography>
+            </div>
+            <div className={styles.deviceNameContainer}>
+              <Typography
+                style={{ fontSize: "0.7rem" }}
+                color="inherit"
+                variant="body2"
+              >
+                {`Playing now on ${playBack.data.device.name}`}
+              </Typography>
+              <div className={styles.iconDeviceContainer}>
+                {playBack.data.device.type.toLowerCase() === "computer" ? (
+                  <ComputerIcon style={{ fontSize: "1rem" }} fontSize="small" />
+                ) : playBack.data.device.type.toLowerCase() === "smartphone" ? (
+                  <SmartphoneIcon
+                    style={{ fontSize: "1rem" }}
+                    fontSize="small"
+                  />
+                ) : (
+                  <SpeakerIcon style={{ fontSize: "1rem" }} fontSize="small" />
+                )}
+              </div>
             </div>
             <div className={styles.refreshButtonContainer}>
               <Button
